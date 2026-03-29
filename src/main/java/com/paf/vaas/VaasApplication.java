09c767756ccc4f29278b8a6b416217c417fcdfc9
Vaas2004 <sahanpasindu2004@gmail.com>
2026-03-29 18:25:06 +0530

    Initial commit

---
+++ src/main/java/com/paf/vaas/VaasApplication.java
@@ -0,0 +1,13 @@
+package com.paf.vaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paf/vaas/VaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paf.vaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
